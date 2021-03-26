e9d9b50dfc5e608dd43842178f4fee14d25179f4
KhanhND <khanhnd162@gmail.com>
2021-03-26 19:03:48 +0700

    Add github packages

--- app/src/main/java/com/kildo/androidbase/MainActivity.java
+++ app/src/main/java/com/kildo/androidbase/MainActivity.java
@@ -4,11 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.kildo.core.Engine;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Engine engine = Engine.instance();
+        engine.start();
     }
 }
